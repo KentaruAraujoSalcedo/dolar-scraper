937f3e06f8c0848b9c83e2b6b9f95c56c8c0114d
KentaruAraujoSalcedo <jaime.araujo.salcedo@gmail.com>
2026-02-22 13:43:14 -0500

    Update chapacambio.py

--- scrapers/chapacambio.py
+++ scrapers/chapacambio.py
@@ -2,7 +2,7 @@ import httpx
 from scrapers.utils import normalize_rate
 
 async def scrap_chapacambio():
-    casa = "ChapaCambio"
+    casa = "chapacambio"
     url = "https://chapacambio.com/"
     endpoint = "https://chapacambio.com/wp-json/chapacambio/tasas"
 
